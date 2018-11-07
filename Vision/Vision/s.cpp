596b6ba767da43a7a9b2d0970cbf4ea9477d8d63
liangdalong <liangdalong2010@gmail.com>
2018-11-07 20:25:11 +0800

    ROI

--- Vision/Vision/s.cpp
+++ Vision/Vision/s.cpp
@@ -1,8 +1,12 @@
 #include <iostream>  
 #include <opencv2/core/core.hpp>  
-#include<opencv2\opencv.hpp>
+#include<opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp> 
 
+
+
+//屏蔽窗口  
+#pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")  
 
 using namespace cv;
 using namespace std;
@@ -11,27 +15,25 @@ int main()
 {
 	// 读入一张图片（poyanghu缩小图）    
 	Mat img = imread("D:\\Project\\CV\\image\\990436704.jpg");
+	Mat logoImage = imread("D:\\Project\\CV\\image\\990436704.jpg");
+
+
+	Mat imageROI(img,Rect(0,0,logoImage.cols,logoImage.rows));
+
+
+	logoImage.copyTo(imageROI, logoImage);
+
 	// 创建一个名为 "图片"窗口    
 	namedWindow("图片");
 	// 在窗口中显示图片   
 	imshow("图片", img);
-	// 等待6000 ms后窗口自动关闭    
-	waitKey(6000);
-	ROI();
-	return 0;
-}
 
-int ROI()
-{
-	Mat srcImage = imread("D:\\Project\\CV\\image\\990436704.jpg");
-	if (!srcImage.data)
-	{
-		cout << "读取图片失败！" << endl;
-		return -1;
-	}
-	rectangle(srcImage, Rect(0, 0, 600, 200), Scalar(0, 255, 0), 2);
-	imshow("ROI", srcImage);
-	waitKey();
-	return 0;
 
+
+
+
+   
+	waitKey(0);
+
+	return 0;
 }
